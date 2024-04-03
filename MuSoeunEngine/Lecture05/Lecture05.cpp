2a9f8b1870b209dc21556235f703bd4b75d802d5
elflee <elflee77@gmail.com>
2024-04-03 10:54:28 +0900

    [ES] 5주차 내용

---
+++ MuSoeunEngine/Lecture05/Lecture05.cpp
@@ -0,0 +1,71 @@
+﻿#pragma comment(lib, "Opengl32.lib")
+
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void errorCallback(int error, const char* description)
+{
+	std::cerr << "GLFW Error: " << description << std::endl;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
+
+int render()
+{
+	glBegin(GL_TRIANGLES);
+	//점1
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(0.0f, 1.0f);
+	//점2
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(0.5f, 0.0f);
+	//점3
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(-0.5f, 0.0f);
+	glEnd();
+	
+	return 0;
+}
+
+
+int main(void)
+{
+	//glfw라이브러리 초기화
+	if (!glfwInit())
+		return -1;
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(1280, 768, "MuSoeunEngine", NULL, NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+	glfwSetErrorCallback(errorCallback);
+	glfwSetKeyCallback(window, keyCallback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		render();
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+
+}
