7fd04a591f2b57c808278cab73f0825737aaeb9a
elflee <elflee77@gmail.com>
2024-04-24 10:52:32 +0900

    .

--- MuSoeunEngine/Lecture07/Lecture07.cpp
+++ MuSoeunEngine/Lecture07/Lecture07.cpp
@@ -1,25 +1,7 @@
-﻿//과제물
-// 1. 이클래스 숙지하고 프로젝트를 생성하시오
-// 2. #include <vector>    대신에 #include "MSList.h" 를 만드시오. (MSList.h, MSList.cpp)
-//    ( Musoen(무소은엔진용) Struct(자료구조) List(리스트) )
-//    ( vector는 절대 쓰지 마시오)
-// 3. MSList 헤더파일 참조해서 cpp 만들것.
-// 4. cpp파일에 함수 구현 내용을 작성하시오. ( ObjectTest처럼 )
-// 
-// 5. 게임에 5주차 과제였던 별을 손톱크기로 조그맣게 그릴 수 있도록 star.hpp의 크리스탈 수정.
-// 6. 랜덤하게 화면에 위치와 색을 바꿔가며 별 300 개를 그릴 수 있도록 작성하시오.
-//    (List에 모든 Star객체들을 담아주세요)
-//    (화면에 알록달록한 별이 가득하게 random 이라는 stl을 사용해서 만들어주세요.)
-
-
-
-#pragma comment(lib, "Opengl32.lib")
+﻿#pragma comment(lib, "Opengl32.lib")
 
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <vector>
-
-#include "Star.hpp"
 
 float moveFactor = 0.0f;
 float scaleFactor = 1.0f;
@@ -45,31 +27,21 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 	}
 }
 
-std::vector<MObject*> list;
+
 
 int initialize()
 {
-	
-	MObject* temp[4];
-	temp[0] = new Star();
-	temp[1] = new Star();
-	temp[2] = new Star();
-	temp[3] = new Star();
 
-	list.push_back(temp[0]);
-	list.push_back(temp[1]);
-	list.push_back(temp[2]);
-	list.push_back(temp[3]);
-		
+
 	return 0;
 }
-int release() 
-{	
+int release()
+{
 	return 0;
 }
 
 int update()
-{	
+{
 	return 0;
 }
 
@@ -78,13 +50,23 @@ int render()
 	glClearColor(0.1f, 0.2f, 0.5f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	for (int i=0;i<4;i++)
-	{
-		MObject* a = list[i];
-		a->render();
-	}
+	glBegin(GL_TRIANGLES);
 
-	
+	glColor3f(0.7f, 0.8f, 0.85f);
+	glVertex2f(0.0f, 0.5f);
+	glColor3f(0.7f, 0.8f, 0.85f);
+	glVertex2f(-0.2f, 0.0f);
+	glColor3f(0.7f, 0.8f, 0.85f);
+	glVertex2f(0.0f, -0.5f);
+
+	glColor3f(0.7f, 0.8f, 0.85f);
+	glVertex2f(0.0f, -0.5f);
+	glColor3f(0.7f, 0.8f, 0.85f);
+	glVertex2f(0.2f, 0.0f);
+	glColor3f(0.7f, 0.8f, 0.85f);
+	glVertex2f(0.0f, 0.5f);
+
+	glEnd();
 
 	return 0;
 }
